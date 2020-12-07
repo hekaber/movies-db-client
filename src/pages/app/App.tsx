11becae15c12ea75312db7fe862cd0028927390d
hkb <hkb@tisynch.com>
2020-12-07 14:34:50 +0100

    moved movies rendering to dedicated component

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,83 +1,17 @@
-import React from 'react';
 import '../../assets/css/App.css';
-import { Thumb } from '../../components/Thumb';
-import { MovieDBService } from '../../services/MovieDBService';
+import { Movies } from '../movies/Movies';
 
-interface State {
-  isLoaded: boolean;
-  items: any[];
-  error: string;
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Movies</h1>
+      </header>
+      <main>
+        <Movies />
+      </main>
+    </div>
+  );
 }
-
-class App extends React.Component<any, State> {
-
-  state: Readonly<State> = {
-    isLoaded: false,
-    items: [],
-    error: ''
-  }
-
-  componentDidMount() {
-    new MovieDBService().getPopularMovies('fr-FR', '1', 'FR').then(
-      (result) => {
-        if (result.success) {
-          this.setState({
-            isLoaded: true,
-            items: result.results
-          });
-        }
-        else {
-          this.setState({
-            isLoaded: false,
-          });
-        }
-      }
-    );
-  }
-
-  renderThumbsContainer() {
-
-    if (this.state.error) {
-      return (<div>Error {this.state.error}</div>)
-    }
-    else if (!this.state.isLoaded) {
-      return (<div>Loading...</div>)
-    }
-    else {
-
-      const movies = this.state.items;
-      const thumbs = movies.map((movie, index) => {
-
-        return (
-          <Thumb
-            key={index}
-            movie={movie}
-            delayTime={index * 500}
-          />
-        );
-      });
-      return (
-        <div className="thumbs_container">
-          {thumbs}
-        </div>
-      )
-    }
-  }
-
-  render() {
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Movies</h1>
-        </header>
-        <main>
-          {this.renderThumbsContainer()}
-        </main>
-      </div>
-    );
-  }
-
-}
-
-export default App;
---
+++ src/pages/movies/Movies.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Thumb } from '../../components/Thumb';
+import { MovieDBService } from '../../services/MovieDBService';
+import '../../assets/css/Movies.css';
+
+interface State {
+    isLoaded: boolean;
+    items: any[];
+    error: string;
+}
+
+export class Movies extends React.Component<any, State>  {
+
+    state: Readonly<State> = {
+        isLoaded: false,
+        items: [],
+        error: ''
+    }
+
+    componentDidMount() {
+
+        new MovieDBService().getPopularMovies('fr-FR', '1', 'FR').then(
+            (result) => {
+                if (result.success) {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.results
+                    });
+                }
+                else {
+                    this.setState({
+                        isLoaded: false,
+                    });
+                }
+            }
+        );
+    }
+
+    render() {
+
+        if (this.state.error) {
+            return (<div>Error {this.state.error}</div>)
+        }
+        else if (!this.state.isLoaded) {
+            return (<div>Loading...</div>)
+        }
+        else {
+
+            const movies = this.state.items;
+            const thumbs = movies.map((movie, index) => {
+
+                return (
+                    <Thumb
+                        key={index}
+                        movie={movie}
+                        delayTime={index * 500}
+                    />
+                );
+            });
+            return (
+                <div className="movies">
+                    <div><h1>Popular Movies</h1></div>
+                    <div className="thumbs_container">
+                        {thumbs}
+                    </div>
+                </div>
+            )
+        }
+    }
+
+}
